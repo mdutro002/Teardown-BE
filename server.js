040204ab7f24f57bac42777d6ad23e48719b232f
Madeline Dutro <dutro.madeline@gmail.com>
2020-07-20 16:18:28 -0400

    debug

--- server.js
+++ server.js
@@ -25,32 +25,31 @@ if (process.env.DATABASE_URL) {
 
   pool.connect();
 
-  } else {
-  console.log('posting locally');
-  pool = new Pool({
-    user: process.env.DBUSER,
-    password: process.env.PASSWORD,
-    port: process.env.PG_PORT,
-    database: process.env.DBNAME
-  })  
-  client = new Client({
-    user: process.env.DBUSER,
-    password: process.env.PASSWORD,
-    port: process.env.PG_PORT,
-    database: process.env.DBNAME
-  });
+  // console.log('posting locally');
+  // pool = new Pool({
+  //   user: process.env.DBUSER,
+  //   password: process.env.PASSWORD,
+  //   port: process.env.PG_PORT,
+  //   database: process.env.DBNAME
+  // })  
+  // client = new Client({
+  //   user: process.env.DBUSER,
+  //   password: process.env.PASSWORD,
+  //   port: process.env.PG_PORT,
+  //   database: process.env.DBNAME
+  // });
 
-  pool.connect((err, client, release) => {
-    if (err){
-      return console.error('Error acquiring client', err.stack)
-    }
-    client.query('SELECT NOW()', (err, result) => {
-      if (err) {
-        return console.error('Error executing query', err.stack)
-      }
-      console.log(result.rows);
-    })
-  });
+  // pool.connect((err, client, release) => {
+  //   if (err){
+  //     return console.error('Error acquiring client', err.stack)
+  //   }
+  //   client.query('SELECT NOW()', (err, result) => {
+  //     if (err) {
+  //       return console.error('Error executing query', err.stack)
+  //     }
+  //     console.log(result.rows);
+  //   })
+  // });
 
 }
 
